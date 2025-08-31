c8603a0f00e00f830fb28d3df2e4935010c67eee
dbprod <driss.boukhamla+1@gmail.com>
2025-08-31 16:45:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Liste des fichiers audio (nom des fichiers doit correspondre à data-sound)
 const sounds = {
-    sound1: 'https://www.cjoint.com/doc/24_11/NKurg1emRVN_coucou-wav.wav',
-    sound2:'https://www.cjoint.com/doc/24_11/NKurlXqVFvN_triple-monstrewav.wav',
-    sound3: 'https://www.cjoint.com/doc/24_11/NKurmThZ82N_je-suis-fatigue-les-brozer-made-with-Voicemod.mp3'
+    sound1: 'coucou-wav.wav',
+    sound2:'triple-monstrewav.wav',
+    sound3: 'je-suis-fatigue-les-brozer-made-with-Voicemod.mp3'
 };
 
 // Ajout d'événements aux boutons
